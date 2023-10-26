3e5d93c4b3709585ef2b3dc9a31b2f10e6ce640e
bijay adhikari <bijay.adhikari.9674227@gmail.com>
2023-10-26 08:11:34 +0545

    Initial Push

---
+++ Source/BattleRoyal/BattleRoyal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BattleRoyal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleRoyal, "BattleRoyal" );
